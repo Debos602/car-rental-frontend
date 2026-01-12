f2bd9b9619aa4721dc14582f15eab0ae03eacaca
Debos Das <debos.das.02@gmail.com>
2026-01-12 22:10:17 +0600

    update tcar schema
    update tcar schema

--- src/types/global.ts
+++ src/types/global.ts
@@ -14,6 +14,7 @@ export interface TCar {
     seats?: number;
     rating?: number;
     fuelType?: string;
+    isElectric?: boolean;
     category?: string;
     location?: string;
     availableFrom?: string;
